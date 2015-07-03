c654e6586771613eb09244d83712ff56919d0102
jump-pi <xpi@enginyers.net>
2015-07-03 02:40:05 +0200

    stand alone test

---
+++ src/com/jumppi/test/StandAloneTest.java
@@ -0,0 +1,23 @@
+package com.jumppi.test;
+
+import com.jumppi.frwk.json.JSON;
+import com.jumppi.frwk.sql.DB;
+import com.jumppi.frwk.sql.DBDescriptor;
+
+public class StandAloneTest {
+
+	public static void main(String[] args) throws Exception {
+		DBDescriptor dbd = DBDescriptor.getInstance();
+		dbd.setProtocol("jdbc:mysql");
+		dbd.setJdbcDriver("com.mysql.jdbc.Driver");
+		dbd.setServer("127.0.0.1");
+		dbd.setDbName("appl");
+		dbd.setUsername("root");
+		DB.addDescriptor("mysql", dbd);
+		DB db = DB.getInstance("mysql");
+
+		JSON res = db.getJSON("select * from ucsim");
+		System.out.println(res.toJsonString());
+	}
+}
+
